7accecd33c2d5cf80396b61eec3079473022a76c
Peder Hatlen <pehaa002@osloskolen.no>
2022-04-05 20:56:22 +0200

    external script fix

--- site/js/webapi.js
+++ site/js/webapi.js
@@ -1,14 +1,15 @@
 async function sendData(e, form, CID, clicks){
-	e.preventDefault();
-
 	let formdata = new FormData(form);
 	formdata.append("cid", CID);
 	formdata.append("ak", clicks);
 
-	await fetch("http://10.2.2.98:8000", {
+	console.log("sending");
+	await fetch("http://10.22.39.100:8000", {
 		method: 'POST',
 		mode: 'no-cors',
 		headers: {'Content-Type' : 'multipart/formdata'},
 		body: formdata,
-	});
+	}).then(e, function(e){
+		console.log(e.status);
+	})
 }
