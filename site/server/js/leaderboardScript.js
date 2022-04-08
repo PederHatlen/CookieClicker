7f74b1053db1b678cd3d80b6f3f46b139c23f935
Peder Hatlen <pehaa002@osloskolen.no>
2022-04-08 17:02:56 +0200

    commenting/cleaning old code

--- site/server/js/leaderboardScript.js
+++ site/server/js/leaderboardScript.js
@@ -10,24 +10,21 @@ let showDebug = true;
 let results;
 let clients;
 
+// Websocket, using socket.io
 const socket = io(`http://${ip}`);
-// const socket = new WebSocket(`ws://api.cookie/socket.io/`)
-
 socket.on("resultat", data => {results.push(data); render();});
 
-// Async function for getting init data, and starting websocket.
-// Async because of fetch which "needs" await
+// Async function for getting init data, async because fetch "needs" await
 async function startup(){
+	// Test data
+	// results = await fetch(`./json/results.json`).then((r)=>{return r.json()});
+	// clients = await fetch(`./json/clients.json`).then((r)=>{return r.json()});
+
 	results = await fetch(`http://${ip}`).then((r)=>{return r.json()});
 	clients = await fetch(`http://${ip}/klienter`).then((r)=>{return r.json()});
-	sort2d(clients, "cid");
+	sort2d(clients, "cid", null, false);
 
 	render();
-}
-
-function socketError(){
-	conInfoEL.innerHTML = "Disconnected :(";
-	conInfoEL.style.background = "red";
 }
 
 // Render function, takes the arrays, sorts them and outputs to table
@@ -40,11 +37,7 @@ function render(){
 	for (let i = 0; i < results.length; i++) results[i]["itemI"] = i;
 	for (let i = 0; i < clients.length; i++) clients[i]["runs"] = 0;
 	
-	// First sort/adding possition and calculating clients
-	let toppsorted = sort2d(Array.from(results), "ak");
-	for (let i = 0; i < toppsorted.length; i++) {
-		toppsorted[i]["pos"] = i+1;
-		clients[toppsorted[i]["cid"]]["runs"] += 1;
+	// Sorting results (timesort is done after, becouse leaderboard possition is also needed).
 	}
 	let timesorted = sort2dts(Array.from(toppsorted), "ts");
 	// Adding the results to the table
@@ -53,23 +46,21 @@ function render(){
 		mainTableEl.innerHTML+= `<tr onclick="remove('${toppsorted[i]["itemI"]}')"><th class=\"placement\">#${toppsorted[i]["pos"]}</th><td class=\"num\">${toppsorted[i]["ak"]}</td><td>${toppsorted[i]["navn"]}</td><td><img src=\"images/ClientImages/${topClientName}.png\"alt=\"${topClientName}\" title=\"${topClientName}\"></td></tr>`;
 		newestTableEl.innerHTML += `<tr onclick="remove('${timesorted[i]["itemI"]}')"><th class=\"placement\">#${timesorted[i]["pos"]}</th><td class=\"num\">${timesorted[i]["ak"]}</td><td>${timesorted[i]["navn"]}</td></tr>`
 	}
-	let runsSorted = sort2d(Array.from(clients), "runs");
+
 	// Client sorting/output
+	let runsSorted = sort2d(Array.from(clients), "runs");
 	for (let i = 0; i < clients.length; i++) {
 		let name = runsSorted[i]["navn"].toLowerCase();
 		clientTableEl.innerHTML += `<tr><th>#${i+1}</th><td class=\"num\">${runsSorted[i]["runs"]}</td><td>${runsSorted[i]["navn"]}</td><td><img src=\"images/ClientImages/${name}.png\"alt=\"${name}\" title=\"${name}\"></td></tr>`;
 	}
 }
 
-// Removing ellements from results (in case offensive ++)
+// Removing ellements from results (in case offensive/other)
 function remove(i){
 	results.splice(i, 1);
 	render();
 }
 
-// 2d sorting functions
-// Timestamp couldn't be sorted in the 2d sort so it needed custom functions
-function sort2d(arr, val, asc = true){return arr.sort((a, b)=>{return asc?(b[val] - a[val]):(a[val] - b[val]);});}
-function sort2dts(arr, val){return arr.sort((a, b)=>{return Date.parse(b[val]) - Date.parse(a[val]);});}
+// 2d sorting function, takes 
 
 startup();
